2f5e4f8fe4b61fd184903b6dda08e30bcc408622
ATurner8133 <156738528+ATurner8133@users.noreply.github.com>
2024-11-19 17:19:19 -0500

    hi

--- 2024 Intern Bot/src/main/java/frc/robot/subsystems/shooter.java
+++ 2024 Intern Bot/src/main/java/frc/robot/subsystems/shooter.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 
@@ -7,8 +9,8 @@ public class shooter extends SubsystemBase{
     public static TalonSRX rightshooter;
     public static TalonSRX leftshooter;
     public shooter() {
-        leftshooter = new TalonSRX(5);
-        rightshooter = new TalonSRX(6);
+        leftshooter = new TalonSRX(Constants.Shooter.leftWheel);
+        rightshooter = new TalonSRX(Constants.Shooter.rightWheel);
 
     }
     @Override
